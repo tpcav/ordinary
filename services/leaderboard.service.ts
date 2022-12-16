1a39dbef252c573624e45b0004be985896c65a16
Andrew Rodebaugh <ajr439@drexel.edu>
2022-12-15 21:31:22 -0500

    leaderboard prototype

---
+++ services/leaderboard.service.ts
@@ -0,0 +1,40 @@
+import firebase from "../utils/firebase";
+import Leaderboard from "../types/leaderboard.type";
+import { ref, set, remove, get, child, query, orderByChild, limitToFirst } from 'firebase/database';
+
+class LeaderboardDataService {
+     async getTopTenStreaks() {
+        const db = ref(firebase, "/leaderboard");
+        const topStreaks = query(db, orderByChild('streak'));
+        const topTenStreaks = query(topStreaks, limitToFirst(10));
+
+        return await get(topTenStreaks);
+    }
+
+  async get(key: string) {
+    const db = ref(firebase);
+    get(child(db, "leaderboard/" + key)).then((snapshot) => {
+        if (snapshot.exists()) {
+          return snapshot.val();
+        } else {
+          console.log("No data available");
+          return null;
+        }
+      }).catch((error) => {
+        console.error(error);
+        return null;
+      });
+  }
+
+  update(key: string, value: Leaderboard) {
+    const db = ref(firebase, "leaderboard/" + key);
+    set(db, value);
+  }
+
+  delete(key: string) {
+    const db = ref(firebase, "leaderboard/" + key);
+    remove(db);
+  }
+}
+
+export default new LeaderboardDataService();
---
+++ types/leaderboard.type.ts
@@ -0,0 +1,7 @@
+export default interface Leaderboard {
+    key?: string | null,
+    date: string,
+    userName: string,
+    streak: Number,
+    score: Number
+}
