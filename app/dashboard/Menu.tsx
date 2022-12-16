d6dc32ef3fec2f403469d8dfd71575074d49abeb
c <thomascavalcante77@gmail.com>
2022-12-16 11:13:08 -0500

    styling changes

--- app/dashboard/Menu.tsx
+++ app/dashboard/Menu.tsx
@@ -25,7 +25,7 @@ export default function Menu() {
           <Popover.Button
             className={classNames(
               open ? 'text-gray-900' : 'text-gray-500',
-              'p-2 group inline-flex items-center rounded-md text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+              'p-2 group inline-flex items-center rounded-md text-base font-medium hover:text-gray-900'
             )}
           >
             <span className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-full'>Menu</span>
