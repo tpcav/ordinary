e19eb3723d9ff81a667ce8589f9593af22cf5276
c <thomascavalcante77@gmail.com>
2023-01-10 17:24:07 -0500

    ui change test

---
+++ pages/test1.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+export default function test1() {
+  return (
+    <div className='h-screen w-screen bg-gray-200 items-center text-center'>
+
+      <div className="">
+        <div className='bg-white p-4'>
+          <h1 className="text-4xl font-black">ORDINARY
+            <span className='text-lg font-semibold'>by Thomas Cavalcante</span>
+          </h1> 
+        </div>
+      </div>
+
+      <div className="grid grid-cols m-auto p-24 justify-center bg-gradient-to-br from-red-200 via-red-300 to-orange-300">
+        <div className="flex justify-center justify-self-center mx-auto text-black text-center text-lg">
+        <div className='flex items-center content-center text-center'>
+            <div className="grid items-center content-center w-100">
+                <div className="inline-flex items-center mt-10">
+                  <div className="flex text-2xl font-semibold items-center w-12 h-12 p-2 bg-gray-200 border-2 border-gray-300">W</div>
+                  <div className="flex text-2xl font-semibold items-center w-12 h-12 p-2 bg-gray-200 border-2 border-gray-300">O</div>
+                  <div className="flex text-2xl font-semibold items-center w-12 h-12 p-2 bg-gray-200 border-2 border-gray-300">R</div>
+                  <div className="flex text-2xl font-semibold items-center w-12 h-12 p-2 bg-gray-200 border-2 border-gray-300">D</div>
+                </div>
+                <div className="inline-flex">
+                  <div className="flex text-2xl font-bold items-center w-12 h-12 p-2 bg-gray-100 border-2 border-gray-300">N</div>
+                  <div className="flex text-2xl font-bold items-center w-12 h-12 p-2 bg-gray-100 border-2 border-gray-300">E</div>
+                  <div className="flex text-2xl font-bold items-center w-12 h-12 p-2 bg-gray-100 border-2 border-gray-300">X</div>
+                  <div className="flex text-2xl font-bold items-center w-12 h-12 p-2 bg-gray-100 border-2 border-gray-300">T</div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols justify-center text-3xl text-white text-center fixed inset-x-0 bottom-0 p-10">
+        <div className="flex justify-center justify-self-center mx-auto text-black text-center text-lg">
+        <div className='flex place-items-center content-center text-center'>
+            <div className="grid items-center content-center w-100">
+                <div className="inline-flex items-center mt-10 justify-self-center text-center">
+                  <div className="flex text-2xl font-semibold place-items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">Q</div>
+                  <div className="flex text-2xl font-semibold grid place-items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">W</div>
+                  <div className="flex text-2xl font-semibold grid items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">E</div>
+                  <div className="flex text-2xl font-semibold items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">R</div>
+                  <div className="flex text-2xl font-semibold items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">T</div>
+                  <div className="flex text-2xl font-semibold items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">Y</div>
+                  <div className="flex text-2xl font-semibold items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">U</div>
+                  <div className="flex text-2xl font-semibold grid place-items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">I</div>
+                  <div className="flex text-2xl font-semibold items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">O</div>
+                  <div className="flex text-2xl font-semibold items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">P</div>
+                </div>
+
+                <div className="inline-flex items-center justify-self-center text-center">
+                  <div className="flex text-2xl font-semibold grid items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">A</div>
+                  <div className="flex text-2xl font-semibold grid items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">S</div>
+                  <div className="flex text-2xl font-semibold grid items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">D</div>
+                  <div className="flex text-2xl font-semibold grid items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">F</div>
+                  <div className="flex text-2xl font-semibold grid items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">G</div>
+                  <div className="flex text-2xl font-semibold grid items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">H</div>
+                  <div className="flex text-2xl font-semibold grid justify-self-center place-items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">J</div>
+                  <div className="flex text-2xl font-semibold grid place-items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">K</div>
+                  <div className="flex text-2xl font-semibold grid place-items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">L</div>
+                </div>
+
+                <div className="inline-flex items-center justify-self-center text-center">
+                  <div className="flex text-2xl font-semibold items-center w-18 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">Enter</div>
+                  <div className="flex text-2xl font-semibold items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">Z</div>
+                  <div className="flex text-2xl font-semibold items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">X</div>
+                  <div className="flex text-2xl font-semibold items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">C</div>
+                  <div className="flex text-2xl font-semibold items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">V</div>
+                  <div className="flex text-2xl font-semibold items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">B</div>
+                  <div className="flex text-2xl font-semibold justify-self-center items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">N</div>
+                  <div className="flex text-2xl font-semibold justify-self-center items-center w-10 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">M</div>
+                  <div className="flex text-2xl font-semibold items-center w-18 h-12 p-2 m-1 rounded-md bg-gray-300 border-2 border-gray-400">Back</div>
+
+                </div>
+              </div>
+          </div>
+
+        </div>
+      </div>
+        
+    </div>
+  )
+}
